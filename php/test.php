1e4d38dab5b4b8929afcf57ec95bd57beb3be9af
SaiRohithEnumala8373 <esairohith@gmail.com>
2023-03-23 16:39:33 -0700

    database test

---
+++ php/test.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<p>Here are some results:</p>
+
+<?php
+
+$host = "cosc360.ok.ubc.ca";
+$database = "db_11505328";
+$user = "11505328";
+$password = "11505328";
+
+
+
+$connection = mysqli_connect($host, $user, $password, $database);
+
+
+$error = mysqli_connect_error();
+if($error != null)
+{
+  $output = "<p>Unable to connect to database!</p>";
+  exit($output);
+}
+else
+{
+    //good connection, so do you thing
+    $sql = "SELECT * FROM users;";
+
+    $results = mysqli_query($connection, $sql);
+
+    //and fetch requsults
+    while ($row = mysqli_fetch_assoc($results))
+    {
+      echo $row['username']." ".$row['firstName']." ".$row['lastName']." ".$row['email']." ".$row['password']."<br/>";
+    }
+
+    mysqli_free_result($results);
+    mysqli_close($connection);
+}
+?>
+</html>
