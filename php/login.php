838c60a147ec96db0a53733da1389a9399cd84a9
SaiRohithEnumala8373 <esairohith@gmail.com>
2023-03-24 14:27:35 -0700

    changed the credentials

--- php/login.php
+++ php/login.php
@@ -5,10 +5,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
       }else{
 
 
-$host = "localhost";
-$database = "project";
-$user = "webuser";
-$password = "P@ssw0rd";
+        $host = "cosc360.ok.ubc.ca";
+        $database = "db_11505328";
+        $user = "11505328";
+        $password = "11505328";
 $connection = mysqli_connect($host, $user, $password, $database);
 $error = mysqli_connect_error();
 if($error != null)
