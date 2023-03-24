798d1ec1113a91b21ea2a4823c4d14ec764327ae
SaiRohithEnumala8373 <esairohith@gmail.com>
2023-03-23 19:31:37 -0700

    Sithara always meri

---
+++ php/newuser.php
@@ -0,0 +1,54 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if(empty($_POST["fullname"]) || empty($_POST["email"]) || empty($_POST["password"])) {
+        die("Input fields are missing");
+      }else{
+
+
+$host = "localhost";
+$database = "project";
+$user = "webuser";
+$password = "P@ssw0rd";
+$connection = mysqli_connect($host, $user, $password, $database);
+$error = mysqli_connect_error();
+if($error != null)
+{
+  $output = "<p>Unable to connect to database!</p>";
+  exit($output);
+}
+else
+{
+    $fullname = $_POST["fullname"];
+     $email = $_POST["email"];
+    $password = md5($_POST["password"]);
+
+     $ds = "SELECT * FROM user where email = '$email'; ";
+     $result = mysqli_query($connection, $ds);
+    if (mysqli_fetch_assoc($result)) {
+      echo "The user with same email already exists";
+      echo "<br>";
+      echo "<a href =".$_SERVER['HTTP_REFERER']." > Return to user entry </a>";
+   }
+   else{
+
+        $statement = "INSERT INTO user (fullname, email, password) VALUES ('$fullname', '$email',  '$password');";
+        $r = mysqli_query($connection, $statement);
+        if ($r) {
+            
+            header("Location: ../html/login.html");
+            
+            
+            exit();
+        
+        }
+    }
+}
+mysqli_free_result($r);
+mysqli_free_result($result);
+mysqli_close($connection);
+}
+}
+else{
+    die("Bad Request");
+}
+?>
