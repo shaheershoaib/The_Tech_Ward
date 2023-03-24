55e44acc3a039ff6355c1e237abfcbc9fc914203
Shaheer <shaheershoaib11@gmail.com>
2023-03-23 18:00:16 -0700

    db

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,7 +9,8 @@ CREATE TABLE `user` (
   
   `fullname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL PRIMARY KEY,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `pfp` BLOB
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -18,7 +19,7 @@ CREATE TABLE `user` (
 CREATE TABLE `discussion` (
  
   
-  `id` int AUTO_INCREMENT PRIMARY KEY,
+  `disucssionId` int AUTO_INCREMENT PRIMARY KEY,
   `title` varchar(255) NOT NULL,
   `description` varchar(1000) NOT NULL,
   `image` BLOB,
@@ -26,3 +27,13 @@ CREATE TABLE `discussion` (
   FOREIGN KEY (email) REFERENCES user(email)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+CREATE TABLE `comment`(
+`commentId` int AUTO_INCREMENT PRIMARY KEY,
+`body` varchar(500) NOT NULL,
+`likeCount` int NOT NULL,
+`discussionId` int NOT NULL,
+`email` varchar(255) NOT NULL,
+FOREIGN KEY (discussionId) REFERENCES discussion(discussionId),
+FOREIGN KEY (email) REFERENCES user(email)
+)
