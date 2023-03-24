c97dfad6e889ae92e6d51263391695c123c301c5
SaiRohithEnumala8373 <esairohith@gmail.com>
2023-03-23 18:40:15 -0700

    database test

--- php/test.php
+++ php/test.php
@@ -24,14 +24,14 @@ if($error != null)
 else
 {
     //good connection, so do you thing
-    $sql = "SELECT * FROM users;";
+    $sql = "SELECT * FROM user;";
 
     $results = mysqli_query($connection, $sql);
 
     //and fetch requsults
     while ($row = mysqli_fetch_assoc($results))
     {
-      echo $row['username']." ".$row['firstName']." ".$row['lastName']." ".$row['email']." ".$row['password']."<br/>";
+      echo $row['fullname']." ".$row['email']."<br/>";
     }
 
     mysqli_free_result($results);
