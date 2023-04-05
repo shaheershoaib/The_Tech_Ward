be302270866c6f1f24dbd62fb3408d0e477c7508
SaiRohithEnumala8373 <esairohith@gmail.com>
2023-04-04 18:05:37 -0700

    Dynamic nav

--- php/pages/show_discussions.php
+++ php/pages/show_discussions.php
@@ -68,8 +68,9 @@ footer{
               <li><a href="new_discussion.php">New Discussion</a></li>
                 <li><a href="show_discussions.php">Discussions</a></li>
                 <li><a href= "account.php">Account</a></li>
-                <li><a href="admin/admin.php">Admin</a></li>
+                
                 <?php session_start(); if(!empty($_SESSION['admin'])) {?>
+                  <li><a href="admin/admin.php">Admin</a></li>
                 <li><a href="admin/search_for_user.php">Search For User</a></li>
                 <?php } ?>
                 <li><a href="../login/logout.php">Logout</a></li> 
