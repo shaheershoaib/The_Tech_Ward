1071a3d7588125128d4428ba8cf718993384115d
Shaheer <shaheershoaib11@gmail.com>
2023-04-07 22:54:23 -0700

    done

--- php/create/newuser.php
+++ php/create/newuser.php
@@ -20,7 +20,7 @@ if($error != null)
 else
 {
     $fullname = $_POST["fullname"];
-     $email = $_POST["email"];
+    $email = $_POST["email"];
     $password = md5($_POST["password"]);
 
      $ds = "SELECT * FROM user where email = '$email'; ";
@@ -34,71 +34,19 @@ else
 
 
     if($_FILES['image']['error'] === UPLOAD_ERR_OK ) {
-           $dir = "pp/";
-            $file = $dir . basename($_FILES["image"]["name"]);
-            $flag = 1;
-            $flag2 = 0;
-            $imageFileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
-            $fileName = $_FILES['image']['name'];
+          
+         
      
-     
-     
-     
-     
-           //  if (file_exists($file)) {
-           //   echo "<br>Duplicate file entry";
-           //   $flag = 0;
-           // }
-     
-           if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "gif" && $imageFileType != "jpeg" ) {
-           echo "<br>Cannot support the uploaded file format.";
-           $flag = 0;
-          }
-     
-          if ($_FILES["image"]["size"] > 500000) {
-           echo "<br>File too large.Max file size is 100K";
-           $flag = 0;
-         }
-     
-     
-     
-     
-          if ($flag == 0) {
-           echo "Sorry, your file was not uploaded.";
-      
-         } else {
-           if (move_uploaded_file($_FILES["image"]["tmp_name"], $file)) {
-             
-            
-             $flag2 = 1;
-           } else {
-             echo "<br> Error Uploading the file";
-           }
-     
-     
-     
-     
-     
-     
-     
-        
-     
-     }
-     
-     if($flag2 == 1){
-      $imagedata = file_get_contents("pp/".$fileName);
-      $sql1 = "INSERT INTO user (fullname, email, password, pfp, contentType) VALUES('$fullname', '$email',  '$password', ?,?);";
-      $stmt = mysqli_stmt_init($connection); 
-      mysqli_stmt_prepare($stmt, $sql1); 
-      $null = NULL;
-      mysqli_stmt_bind_param($stmt, "bs", $null, $imageFileType);
-      mysqli_stmt_send_long_data($stmt, 0, $imagedata);
-      $resultt = mysqli_stmt_execute($stmt) or die(mysqli_stmt_error($stmt));
+      $file_contents = file_get_contents($_FILES['image']['tmp_name']);
+      $image = mysqli_escape_string($connection, $file_contents);
+      $contentType = strtolower( pathinfo(basename($_FILES['image']['name']), PATHINFO_EXTENSION  )  );
+      $sql = "INSERT INTO user (fullname, email, password, pfp, contentType) VALUES('$fullname', '$email',  '$password', '$image', '$contentType');";
+      mysqli_query($connection, $sql);
       header("Location: ../../html/login.html");
+    }
       
-      }
      
- }
+
 else{
         $statement = "INSERT INTO user (fullname, email, password) VALUES ('$fullname', '$email',  '$password');";
         $r = mysqli_query($connection, $statement);
