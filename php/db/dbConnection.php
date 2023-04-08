861c766916b3c00e49547679fe27388de57422aa
Shaheer <shaheershoaib11@gmail.com>
2023-04-07 23:02:09 -0700

    m

--- php/db/dbConnection.php
+++ php/db/dbConnection.php
@@ -16,15 +16,15 @@ private $error;
 
     public function __construct()
     {
-         $this->host = "localhost";
-         $this->database = "project";
-         $this->user = "webuser";
-         $this->password = "P@ssw0rd";
+         //$this->host = "localhost";
+         //$this->database = "project";
+         //$this->user = "webuser";
+         //$this->password = "P@ssw0rd";
 
-       // $this->host = "cosc360.ok.ubc.ca";
-       // $this->database = "db_11505328";
-       // $this->user = "11505328";
-       // $this->password = "11505328";
+        $this->host = "cosc360.ok.ubc.ca";
+        $this->database = "db_84766492";
+        $this->user = "84766492";
+        $this->password = "84766492";
 
         $this->connection = mysqli_connect($this->host, $this->user, $this->password, $this->database);
         $this->error = mysqli_connect_error();
